5c055271846fe13fc1a2cdd07cbfb89d4c7e6e42
Colvin Zhu <CWZhu@github.com>
2017-11-11 14:34:52 -0500

    adds TwilioClient

--- src/main/java/com/trivago/hackathon/roaming/RoamingApplication.java
+++ src/main/java/com/trivago/hackathon/roaming/RoamingApplication.java
@@ -1,5 +1,7 @@
 package com.trivago.hackathon.roaming;
 
+import com.trivago.hackathon.roaming.twilio.TwilioClient;
+import com.twilio.Twilio;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +9,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RoamingApplication implements CommandLineRunner {
 
+	private static final String ACCTSID = "AC46310db91a77ed3b9bfb7ee89d8bbfd1";
+	private static final String AUTHTKN = "8e66bb2a8ef84c16e784273a401d239d";
+
 	public static void main(String[] args) {
 		SpringApplication.run(RoamingApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
+		Twilio.init(ACCTSID, AUTHTKN);
+		//send a message
+		// TwilioClient.send("+14703497823", "+16177102946", "TestMessageTwilioClient");
 	}
 }
