280fd5e04a7adbda326d1ccd60d20c6c52c82f64
Josh Dev <1900843+JoshDev@users.noreply.github.com>
2017-11-11 16:27:22 -0500

    added boilerplate

--- src/main/java/com/trivago/hackathon/roaming/rest/MessageReceiverController.java
+++ src/main/java/com/trivago/hackathon/roaming/rest/MessageReceiverController.java
@@ -3,6 +3,8 @@ package com.trivago.hackathon.roaming.rest;
 /**
  * Created by khadka on 11/11/17.
  */
+import com.trivago.hackathon.roaming.automation.TrivagoBrowser;
+import com.trivago.hackathon.roaming.model.SearchResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.twilio.twiml.Body;
@@ -12,14 +14,23 @@ import com.twilio.twiml.MessagingResponse;
 
 import com.twilio.twiml.TwiMLException;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/receive")
 public class MessageReceiverController {
 
+    TrivagoBrowser browser;
+    Map<String, List<SearchResult>> resultsCache;
+
     @Autowired
     MessageReceiverController(){
-
+        browser = new TrivagoBrowser();
+        resultsCache = new HashMap<>();
     }
+
     @RequestMapping(method = RequestMethod.POST, produces={"application/xml"})
     //String receive (@RequestBody String  msgbody) throws TwiMLException {
     String receive (@RequestParam("From") String from, @RequestParam("Body") String body,
@@ -27,16 +38,63 @@ public class MessageReceiverController {
 
         System.out.println(from + ":"+body+":"+fromZip);
 
-        MessagingResponse twiml;
-        Message sms = new Message.Builder()
-                    .body(new Body("Hello, Mobile Monkey"))
-                    .build();
-        twiml = new MessagingResponse
-                    .Builder()
-                    .message(sms)
-                    .build();
-       return twiml.toXml();
+        if (body.contains("trivago")) {
+            return helloMessage();
+        } else if (body.startsWith("search ")) {
+            List<SearchResult> results = browser.getSearchResults(body.split(" ")[1]);
+            resultsCache.put(from, results);
+            return listResults(results);
+        } else if (body.startsWith("info ")) {
+            if (!resultsCache.containsKey(from)) {
+                return invalid();
+            } else {
+                return moreInfo(body.split(" ")[1]);
+            }
+        } else if (body.startsWith("book ")) {
+            if (!resultsCache.containsKey(from)) {
+                return invalid();
+            } else {
+                return book(from, resultsCache.get(from).get(Integer.valueOf(body.split(" ")[1])));
+            }
+        }
+        else {
+            return invalid();
+        }
     }
 
+    private String book(String from, SearchResult result) throws TwiMLException {
+        browser.clickLink(result);
+        resultsCache.remove(from);
+        return buildMessage("booked!");
+    }
+
+    private String moreInfo(String index) throws TwiMLException {
+        return buildMessage("More info here"):
+    }
+
+    private String listResults(List<SearchResult> results) throws TwiMLException {
+
+        return buildMessage("here are the results");
+    }
+
+    private String invalid() throws TwiMLException {
+        return buildMessage("Invalid option, try again");
+    }
+
+    private String helloMessage() throws TwiMLException {
+        return buildMessage("type search [query] find hotels");
+    }
+
+    String buildMessage(String message) throws TwiMLException {
+        MessagingResponse twiml;
+        Message sms = new Message.Builder()
+                .body(new Body(message))
+                .build();
+        twiml = new MessagingResponse
+                .Builder()
+                .message(sms)
+                .build();
+        return twiml.toXml();
+    }
 
 }
